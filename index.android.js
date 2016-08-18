8a7acc40fd9d5c6e79b44e5ff2a00f3a9d38030d
Fabian Köster <mail@fabian-koester.com>
2016-08-18 15:52:09 +0200

    Upgrade to latest react,[-native] version

--- index.android.js
+++ index.android.js
@@ -2,4 +2,7 @@ import { AppRegistry } from 'react-native';
 
 import NestedTabsDemo from './NestedTabsDemo';
 
+// FIXME
+console.ignoredYellowBox = ['Warning: You are manually calling a React.PropTypes'];
+
 AppRegistry.registerComponent('NestedTabsDemo', () => NestedTabsDemo);
